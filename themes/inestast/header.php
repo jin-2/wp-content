3ca3dedbaa6337f7e115d0fc827738a382e9ace2
jin-2 <jmakeaz@gmail.com>
2015-10-17 17:09:10 +0900

    - 웹폰트 적용 Noto Sans KR
    - 메인 더미 이미지 삽입
    - 웹폰트 적용 Noto Sans KR
    — style.css: import
    — hearer.php: 스타일 적용

--- themes/inestast/header.php
+++ themes/inestast/header.php
@@ -1,15 +1,35 @@
-<?php
-/**
- * @package Inesta
-*/  
-?>
-<!DOCTYPE HTML>
-<html <?php language_attributes(); ?>>
-	<head>
-		<!-- META DATA -->
-		<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">	
-		<?php wp_head(); ?> 
-	</head>
+<?php
+
+/**
+
+ * @package Inesta
+
+*/  
+
+?>
+
+<!DOCTYPE HTML>
+
+<html <?php language_attributes(); ?>>
+
+	<head>
+
+		<!-- META DATA -->
+
+		<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+
+		<meta charset="UTF-8">
+
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">	
+
+		<?php wp_head(); ?> 
+
+		<style>
+			body, h1, h2, h3, h4, h5, h6{
+				font-family: "Noto Sans KR";
+			}
+		</style>
+
+	</head>
+
 	<body <?php body_class();?>> 
